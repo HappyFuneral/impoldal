5996fb8077fa53b5074e214db2e83b190061bf99
Milan Maroevich <milan.maroevich@gmail.com>
2024-12-06 02:16:12 +0100

    mixed content

--- reactlaravel/resources/views/app.blade.php
+++ reactlaravel/resources/views/app.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Impardonné') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <meta http-equiv="Content-Security-Policy" content="block-all-mixed-content">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
