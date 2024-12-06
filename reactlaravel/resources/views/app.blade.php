6ee815a0cd0520078c0b800dd459e6246b2ef5e1
Milan Maroevich <milan.maroevich@gmail.com>
2024-12-06 16:13:58 +0100

    cross origin allow

--- reactlaravel/resources/views/app.blade.php
+++ reactlaravel/resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<?php header("Access-Control-Allow-Origin: *");?>
         <title inertia>{{ config('app.name', 'Impardonné') }}</title>
 
         <!-- Fonts -->
