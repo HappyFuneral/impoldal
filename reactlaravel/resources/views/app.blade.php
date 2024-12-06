bbe802a7c7c51689cda6dc2aed522cd7a1194c22
Milan Maroevich <milan.maroevich@gmail.com>
2024-12-06 16:11:39 +0100

    update insecure requests

--- reactlaravel/resources/views/app.blade.php
+++ reactlaravel/resources/views/app.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <meta http-equiv="Content-Security-Policy" content="block-all-mixed-content">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <!-- Scripts -->
         @routes
         @viteReactRefresh
